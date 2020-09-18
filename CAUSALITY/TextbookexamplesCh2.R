6ada4732c395817cebee38166846ada1347c673e
jdunca <jamie.duncan@mail.utoronto.ca>
2020-09-18 13:19:53 -0400

    cont

--- CAUSALITY/TextbookexamplesCh2.R
+++ CAUSALITY/TextbookexamplesCh2.R
@@ -139,5 +139,85 @@ tapply(social$primary2004, social$messages, mean)
 
 tapply(social$hhsize, social$messages, mean)
 
+######minimum wage example
+
+minwage <- read.csv("minwage.csv", stringsAsFactors = TRUE)
+summary(minwage)
+
+###subsetting data into two tables
+
+minwageNJ <- subset(minwage, subset = (location != "PA"))
+minwagePA <- subset(minwage, subset = (location == "PA"))
+
+mean(minwageNJ$wageBefore < 5.05)
+mean(minwageNJ$wageAfter < 5.05)
+mean(minwagePA$wageBefore < 5.05)
+mean(minwagePA$wageAfter < 5.05)
 
 
+###create a variable from proportion of fulltime employees in NJ and PA
+minwageNJ$fullPropAfter <- minwageNJ$fullAfter / (minwageNJ$fullAfter + minwageNJ$partAfter)
+minwagePA$fullPropAfter <- minwagePA$fullAfter / (minwagePA$fullAfter + minwagePA$partAfter)
+
+mean(minwageNJ$fullPropAfter) - mean(minwagePA$fullPropAfter)
+
+
+###confounding variables and statistical control
+
+prop.table(table(minwageNJ$chain))
+prop.table(table(minwagePA$chain))
+
+minwageNJ.bk <- subset(minwageNJ, subset = (chain == "burgerking"))
+minwagePA.bk <- subset(minwagePA, subset = (chain == "burgerking"))
+
+mean(minwageNJ.bk$fullPropAfter) - mean(minwagePA.bk$fullPropAfter)
+
+##location as confounding variable?
+
+minwage.bk.subset <-  subset(minwageNJ.bk, subset = ((location != "shoreNJ") & (location != "centralNJ")))
+mean(minwage.bk.subset$fullPropAfter) - mean(minwagePA.bk$fullPropAfter)
+
+
+### before-and-after design
+##f/t prop in prev period
+minwageNJ$fullPropBefore <- minwageNJ$fullBefore / (minwageNJ$fullBefore + minwageNJ$partBefore)
+NJdiff <- mean(minwageNJ$fullPropAfter) - mean(minwageNJ$fullPropBefore)
+NJdiff
+
+
+minwagePA$fullPropBefore <- minwagePA$fullBefore / (minwagePA$fullBefore + minwagePA$partBefore)
+PAdiff <- mean(minwagePA$fullPropAfter) - mean(minwagePA$fullPropBefore)
+PAdiff
+NJdiff - PAdiff
+
+###Descriptive statistics
+##quantiles
+
+median(minwageNJ$fullPropAfter) - median(minwagePA$fullPropAfter)
+NJdiff.med <- median(minwageNJ$fullPropAfter) - median(minwageNJ$fullPropBefore)
+NJdiff.med
+PAdiff.med <- median(minwagePA$fullPropAfter) - median(minwagePA$fullPropBefore)
+NJdiff.med - PAdiff.med
+
+summary(minwageNJ$wageBefore)
+summary(minwageNJ$wageAfter)
+IQR(minwageNJ$wageBefore)
+IQR(minwageNJ$wageAfter)
+
+###other quantiles
+##deciles
+quantile(minwageNJ$wageBefore, probs = seq(from = 0, to = 1, by = 0.1))
+quantile(minwageNJ$wageAfter, probs = seq(from = 0, to = 1, by = 0.1))
+
+
+###standard deviation
+##RMS -- root mean square
+sqrt(mean((minwageNJ$fullPropAfter - minwageNJ$fullPropBefore)^2))
+mean(minwageNJ$fullPropAfter - minwageNJ$fullPropBefore)
+
+sd(minwageNJ$fullPropBefore)
+
+sd(minwageNJ$fullPropAfter)
+
+var(minwageNJ$fullPropBefore)
+var(minwageNJ$fullPropAfter)   
