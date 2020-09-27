21010779a6752f26cb0860202950afa16fe0b974
jdunca <jamie.duncan@mail.utoronto.ca>
2020-09-27 13:02:56 -0400

    working

--- MEASUREMENT/Problem Set 2 (Sept 29)/PS2 Notes.R
+++ MEASUREMENT/Problem Set 2 (Sept 29)/PS2 Notes.R
@@ -2,20 +2,121 @@
 ## Question 1
 
 ### 1.1.1: Show that the treatment and control groups are balanced with respect to the income variable (income) by comparing its distribution between those in the treatment group and those in the control group. 
-### 1.1.2: Also, compare the proportion of males (male) in the treatment and control groups. 
 
+summary(as.factor(boston$income))
+summary(boston$male)
+as.integer(3e+05)
+
+co.var1 <- prop.table(table(income = boston$income, treatment = boston$treatment))
+co.var1
+
+treated <- subset(boston, subset = (treatment == 1))
+not_treated <- subset(boston, subset = (treatment == 0))
+
+summary(treated$income)
+summary(not_treated$income)
+
+(mean(treated$numberim.pre) - mean(treated$numberim.post, na.rm = TRUE))
+(mean(not_treated$numberim.pre, na.rm = TRUE) - mean(not_treated$numberim.post, na.rm = TRUE))
+
+0.2686543/5
+
+boston %>%
+  group_by(income, male) %>%
+  summarise(diff = mean(treatment, na.rm = T)) %>%
+  mutate(diff2 = diff - lag(diff))
+
+
+?lag
+
+boston
+
+%>%
+  summarise(diff = mean(treatment, na.rm = T)) %>%
+  mutate(diff2 = diff - lag(diff))
+
+
+
+### 1.1.2: Also, compare the proportion of males (male) in the treatment and control groups. 
+co.var2 <- prop.table(table(male = boston$male, treatment = boston$treatment))
+co.var2
 ### 1.1.3: Interpret these two numbers.
+1
+
 ## Question 2
 
 ### 1.2.1: Compute the average treatment effect on the change in attitudes about immigration. That is, how does the mean change in attitudes about immigration policy for those in the control group compare to those in the treatment group.
+
+treated.shift <- (mean(treated$numberim.post - treated$numberim.pre, na.rm = TRUE))
+
+nottreated.shift <- (mean(not_treated$numberim.post - not_treated$numberim.pre, na.rm = TRUE))
+
+treated.shift - nottreated.shift
 
 ### 1.2.2: Interpret the result.
 
 ## Question 3
 ### Does having attended college influence the effect of being exposed to outsiders on exclusionary attitudes? Another way to ask the same question is this: is there evidence of a differential impact of treatment, conditional on attending college versus not attending college? 
+
+  ### 1.3.1: Calculate the necessary quantities to answer this question and interpret the results. 
+
+sate <- function(df){
+treated <- subset(df, subset = (treatment ==1 ))
+not_treated <- subset(df, subset = (treatment == 0))
+treated.shift <- (mean(treated$numberim.post, na.rm =TRUE) - mean(treated$numberim.pre, na.rm = TRUE))
+nottreated.shift <- (mean(not_treated$numberim.post, na.rm = TRUE) - mean(not_treated$numberim.pre, na.rm = TRUE))
+sate <- treated.shift - nottreated.shift
+return(sate)
+}
+
+sate(boston)
+
+
+
+
+
+
+
+
+boston %>%
+  group_by(college) %>%
+  summarise(sate)
+
+
+##subset by treated / not treated
+treated <- subset(boston, subset = (treatment == 1))# subset into treated 
+not_treated <- subset(boston, subset = (treatment == 0))#and not treated
+
+### subset again by college/ no college
+college.tr <- subset(treated, subset = (college == 1))# subset into treated 
+college.nt <- subset(not_treated, subset = (college == 1))#and not treated
+no.college.tr <- subset(treated, subset = (college == 0))
+no.college.nt <- subset(not_treated, subset = (college == 0))
+
+coll_shift <- (mean(college.tr$numberim.pre - college.tr$numberim.post, na.rm = T) - (mean(college.nt$numberim.pre - college.nt$numberim.post, na.rm = T)
+nocoll_shift <- (mean(no.college.tr$numberim.pre - no.college.tr$numberim.post, na.rm = T) - (mean(no.college.nt$numberim.pre - no.college.nt$numberim.post, na.rm = T))
+coll_shift - nocoll_shift
+
+
+boston %>%
+group_by(college) %>%
+  summarise(mean(treatment)) %>%
+  filter((shift = mean(numberim.pre) - mean(numberim.post)))
+
+
+
+shift.sate <- function(parameter){
+  treated <- subset(boston$parameter == 1, parameter ==)
+}
   
-  ### 1.3.1: Calculate the necessary quantities to answer this questionand interpret the results. 
+
+
+###function steps
+##
+ 
+
   
+
   ### 1.3.2: Consider the average treatment effect for those who attended college and then those who did not.
   
   ## Question 4
@@ -23,13 +124,20 @@
   ### Repeat the same analysis as in the previous question but this time with respect to age and ideology. 
   
   ### 1.4.1: For age, divide the data based on its quartile and compute the average treatment effect within each of the resulting four groups. 
-  ### 1.4.2: For ideology, compute the average treatment effect within each value. 
+
+age_group <- quantile(boston$age)
+age_group
+
+boston$age
+
+### 1.4.2: For ideology, compute the average treatment effect within each value. 
 
 ### 1.4.3: What patterns do you observe? Give a brief substantive interpretation of the results.
 
 # Section 2
 ## Question 1
 ### 2.1.1: How many members of parliament are in the data? 
+summary(as.factor(prop.AJPS$election))
 
 ### 2.1.2: Why is it not simply 338 x 2 = 676?
 
